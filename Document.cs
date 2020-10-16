f051a94a5ca30a36335048a0ca476d17251f4e87
Shawn C <scooter4356@gmail.com>
2020-10-16 16:44:19 -0500

    Add project files.

---
+++ Document.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2J_Calc
+{
+    class Document
+    {
+        int numPages;
+        int numFields;
+        bool needFast;
+        bool needWord;
+
+        public Document(int numPages, int numFields, bool needFast, bool needWord)
+        {
+            this.numPages = numPages;
+            this.numFields = numFields;
+            this.needFast = needFast;
+            this.needWord = needWord;
+        }
+    }
+}
