533b5ea63d10bd3f1559e4fa5a1b9976dba11f66
Shawn C <scooter4356@gmail.com>
2020-10-16 17:53:15 -0500

    have basic doc object created through form submit

---
+++ Doc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2J_Calc
+{
+    class Doc
+    {
+        int numPages;
+        int numFields;
+        int numRepeated;
+        bool needFast;
+        bool needWord;
+
+        public Doc(int numPages, int numFields, int numRepeated, bool needFast, bool needWord)
+        {
+            this.numPages = numPages;
+            this.numFields = numFields;
+            this.numRepeated = numRepeated;
+            this.needFast = needFast;
+            this.needWord = needWord;
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            output += "numPages:" + this.numPages;
+            output += "\nnumFields:" + this.numFields;
+            output += "\nnumRepeated:" + this.numRepeated;
+            output += "\nneedFast:" + this.needFast;
+            output += "\nneedWord" + this.needWord;
+
+
+            return output;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,10 +26,59 @@ namespace A2J_Calc
 
             int totalCost;
             int docCost;
+            List<Doc> masterList;
+
+
+            //Init starting values
+            txtDocNumber.Text = "1";
+            txtDocNumber.IsReadOnly=true;
+
+
+            txtNumPages.Text = "1";
+            txtNumRepeated.Text = "0";
+
+            txtNumFields.Text = "0";
+            boolNeedFast.IsChecked = false;
+            boolNeedWord.IsChecked = false;
             
+
 
         }
 
-       
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            //int test = Int32.Parse(txtNumPages.Text);
+            //MessageBox.Show(test);
+            int numPages;
+            if (!Int32.TryParse(txtNumPages.Text, out numPages))
+            {
+                MessageBox.Show("The number of pages must be a whole number. Thank you");
+            }
+
+            int numFields;
+            if (!Int32.TryParse(txtNumFields.Text, out numFields))
+            {
+                MessageBox.Show("The number of fields must be a whole number. Thank you");
+            }
+
+            int numRepeated;
+            if (!Int32.TryParse(txtNumRepeated.Text, out numRepeated))
+            {
+                MessageBox.Show("The number of fields repeated must be a whole number. Thank you");
+            }
+
+            bool needFast = boolNeedFast.IsChecked ?? false;
+
+            bool needWord = boolNeedWord.IsChecked ?? false;
+
+            Doc test = new Doc(numPages, numFields, numRepeated, needFast, needWord);
+
+            MessageBox.Show(test.ToString());
+        }
     }
 }
